38bc607e685082a74d94c4311c16c1941b010370
Mike Sutherland <33564709+rland93@users.noreply.github.com>
2026-04-18 00:39:47 -0700

    Add: longest-common-prefix - submission-4

---
+++ Data Structures & Algorithms/longest-common-prefix/submission-4.rs
@@ -0,0 +1,11 @@
+impl Solution {
+    pub fn longest_common_prefix(strs: Vec<String>) -> String {
+        let mut prefix = &strs[0][..];
+        for s in &strs[1..] {
+            while !s.starts_with(prefix) {
+                prefix = &prefix[..prefix.len() - 1];
+            }
+        }
+        return prefix.to_string();
+    }
+}
